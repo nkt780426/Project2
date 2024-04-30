30bb44f71178a6ed94266c1ef7bfae6c7c5852c7
hoangvo <thoatkhoinicktao2002@gmail.com>
2024-05-01 00:30:36 +0700

    Finish final project

--- 4. final project/main.py
+++ 4. final project/main.py
@@ -14,6 +14,7 @@ capture = cv.VideoCapture(mjpeg_url)
 # Biến đếm tổng số lần cười
 num_smiles = 0
 smile_text = None
+color = None
 
 while True:
     # Đọc từng frame, ret là bool nếu đọc được frame
@@ -22,30 +23,34 @@ while True:
         print("Failed to capture frame")
         break
     
+    # Chuyển ảnh về grayscale mới dùng được model + haar_cascade
+    grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # Toạ độ các mặt trong frame
-    faces = detect_faces(frame)
+    faces = detect_faces(grayFrame)
     
     # Lặp qua các khuôn mặt được phát hiện
     for (x, y, w, h) in faces:
         # Cắt ảnh khuôn mặt
-        face_img = frame[y:y+h, x:x+w]
+        face_img = grayFrame[y:y+h, x:x+w]
         # Dự đoán cười trên ảnh khuôn mặt
         prediction = predict_smile(face_img)
         # Nếu có cười, tăng biến đếm và vẽ hình chữ nhật xung quanh khuôn mặt
         if prediction > 0.5:
             smile_text = 'Smiling'
+            color = (0, 255, 0)
             num_smiles += 1
         else:
             smile_text = 'Not smiling'
+            color = (0, 0, 255)
         # Vẽ xung quanh faces phát hiện được và hiển thị kết quả predictions
         cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv.putText(frame, smile_text, (x, y-10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        cv.putText(frame, smile_text, (x, y-10), cv.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
     
     # Hiển thị tổng số lần cười từ đầu video
     cv.putText(frame, f'Smiles: {num_smiles}', (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     
     # Hiển thị video, nhấn 'q' để thoát project
-    cv.imshow('Face Detection', frame)
+    cv.imshow('Smile Face Detection', frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         print('Exit')
         break
--- 4. final project/readme.md
+++ 4. final project/readme.md
@@ -1,5 +1,4 @@
-https://learn.microsoft.com/en-us/windows/wsl/connect-usb
-https://github.com/phuoctan4141/WSL/blob/main/Connect%20USB%20devices/USB%20Camera.md
-https://github.com/dorssel/usbipd-win
-
 Get-NetIPAddress | Where-Object {$_.AddressFamily -eq "IPv4" -and $_.PrefixLength -eq "24"} | Select-Object -ExpandProperty IPAddress
+
+Nhược điểm: 
+- Cười đau mặt mới tính là cười :))
